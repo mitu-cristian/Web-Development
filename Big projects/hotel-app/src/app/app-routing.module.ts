d28188cb0f72ae3eca429c40c4af256fa8b92c21
mitu-cristian <18.mit9144@gmail.com>
2023-02-04 23:57:51 +0200

    .

--- Big projects/hotel-app/src/app/app-routing.module.ts
+++ Big projects/hotel-app/src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
     path: 'booking', component: BookingComponent,
-    canActivate: [AuthGuard]
+    // canActivate: [AuthGuard]
   }
 ];
 
