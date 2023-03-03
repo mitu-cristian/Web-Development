8120cd56970fb3fbbf548ea35473020091c27b36
mitu-cristian <18.mit9144@gmail.com>
2023-03-03 16:13:46 +0200

    Project finished
    Making the page responsive to all types of screen + showing an animation when loading.

--- Small projects/404/assets/js/main.js
+++ Small projects/404/assets/js/main.js
@@ -38,3 +38,11 @@ function scrollHeader() {
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    distance: '90px',
+    duration: 3000
+})
+
+sr.reveal(`.home__data`, { origin: 'top', delay: 400 })
+sr.reveal(`.home__img`, { origin: 'bottom', delay: 600 })
+sr.reveal(`.home__footer`, { origin: 'bottom', delay: 600 })
