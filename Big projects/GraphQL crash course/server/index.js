c7b4ec06671619a4d18f9a5c30608edc253733da
mitu-cristian <18.mit9144@gmail.com>
2023-08-10 23:28:00 +0300

    CRUD backend & delete for frontend

---
+++ Big projects/GraphQL crash course/server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+require('dotenv').config();
+const {graphqlHTTP} = require("express-graphql");
+const schema = require('./schema/schema');
+const connectDB = require ("./config/db");
+const cors = require ("cors");
+
+const port = process.env.PORT || 5000;
+
+const app = express ();
+
+// Connect to database
+connectDB();
+
+// CORS
+app.use(cors());
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}))
+
+app.listen(port, console.log(`Server running on ${port}.`))
